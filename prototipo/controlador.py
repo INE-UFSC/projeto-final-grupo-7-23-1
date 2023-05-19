47d60ea6ad2f927440f04893eca1782b7c994380
Pedro Casotti <pedro.c.casotti@gmail.com>
2023-05-19 10:23:49 -0300

    Cria classe Controlador

---
+++ prototipo/controlador.py
@@ -0,0 +1,34 @@
+import pygame
+
+from estado import Estado
+
+FPS = 60
+
+class Controlador:
+    def __init__(self):
+        self._estado = Estado()
+        self.__entidades = []
+
+    def run(self):
+        pygame.init()
+        screen = pygame.display.set_mode((1280, 720))
+        clock = pygame.time.Clock()
+
+        running = True
+        dt = 0
+
+        while running:
+            self.__update(screen, dt);
+            dt = clock.tick(FPS) / 1000
+
+        pygame.quit()
+
+    def __update(self, screen: pygame.Surface, dt: float) -> bool:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+
+        screen.fill("black")
+        pygame.display.flip()
+
+        return True
