800c4b91f21ab3c2679ebaf209c8068887a47895
Hugo <hugomatosdecarvalho@gmail.com>
2023-07-03 11:25:34 -0300

    Curupira e rearranjo menu personagem

--- versao_final/menus/menu_personagem.py
+++ versao_final/menus/menu_personagem.py
@@ -7,9 +7,8 @@ class MenuPersonagem(Menu):
         super().__init__(controlador)
         self.__state = "Personagem1"
         self.__escolhido = 1
-        self.__p1x, self.__p1y = 260, TELA_HEIGHT / 2 + 30
-        self.__p2x, self.__p2y = TELA_WIDTH / 2, TELA_HEIGHT / 2 + 30
-        self.__p3x, self.__p3y = 1020, TELA_HEIGHT / 2 + 30
+        self.__p1x, self.__p1y = 400, TELA_HEIGHT / 2 + 30
+        self.__p2x, self.__p2y = TELA_WIDTH - 400, TELA_HEIGHT / 2 + 30
         self.__voltarx, self.__voltary = TELA_WIDTH / 2, TELA_HEIGHT - 100
         self.__offset_p = -82.5
         self.__offset_voltar = -150
@@ -18,16 +17,13 @@ class MenuPersonagem(Menu):
         self.__personagem1 = [[image.load(CAMINHO_ASSETS + "saci1.png"), image.load(CAMINHO_ASSETS + "saci2.png"),
                               image.load(CAMINHO_ASSETS + "saci3.png"), image.load(CAMINHO_ASSETS + "saci4.png")],
                               [image.load(CAMINHO_ASSETS + "saciagachado.png")]]
-        self.__personagem2 = [image.load(CAMINHO_ASSETS + "box1.png"), image.load(CAMINHO_ASSETS + "box2.png")]
-        self.__personagem3 = [image.load(CAMINHO_ASSETS + "powerup.png")]
+        self.__personagem2 = [[image.load(CAMINHO_ASSETS + "curupira1.png")], [image.load(CAMINHO_ASSETS + "curupira2.png")]]
         self.__imagem_p1 = transform.scale(self.__personagem1[0][0], (100, 200))
-        self.__imagem_p2 = transform.scale(self.__personagem2[0], (100, 200))
-        self.__imagem_p3 = transform.scale(self.__personagem3[0], (100, 200))
+        self.__imagem_p2 = transform.scale(self.__personagem2[0][0], (100, 200))
         self.__cor_padrao = "white"
         self.__cor_selecionado = "green"
         self.__cor_borda_p1 = self.__cor_selecionado
         self.__cor_borda_p2 = self.__cor_padrao
-        self.__cor_borda_p3 = self.__cor_padrao
         self.get_cursor_rect().midtop = (self.__p1x + self.__offset_p, self.__p1y)
     
     def display_menu(self, estado):
@@ -41,17 +37,13 @@ class MenuPersonagem(Menu):
             self.get_controlador().draw_text("PERSONAGEM", 60, TELA_WIDTH/ 2, 180, "green")
             self.__botao_p1 = self.get_controlador().draw_rect(self.__rect_borda, self.__p1x, self.__p1y, self.__cor_borda_p1)
             self.__botao_p2 = self.get_controlador().draw_rect(self.__rect_borda, self.__p2x, self.__p2y, self.__cor_borda_p2)
-            self.__botao_p3 = self.get_controlador().draw_rect(self.__rect_borda, self.__p3x, self.__p3y, self.__cor_borda_p3)
             self.get_controlador().draw_rect(self.__rect_fundo, self.__p1x, self.__p1y, "blue")
             self.get_controlador().draw_rect(self.__rect_fundo, self.__p2x, self.__p2y, "blue")
-            self.get_controlador().draw_rect(self.__rect_fundo, self.__p3x, self.__p3y, "blue")
             self.get_controlador().draw_image(self.__imagem_p1, self.__p1x, self.__p1y)
             self.get_controlador().draw_image(self.__imagem_p2, self.__p2x, self.__p2y)
-            self.get_controlador().draw_image(self.__imagem_p3, self.__p3x, self.__p3y)
             self.get_controlador().draw_text("Escolhido", 15, self.__p1x + (self.__escolhido - 1) * (self.__p2x - self.__p1x), self.__p1y - 125, "green")
             self.get_controlador().draw_text("Saci-pererê", 25, self.__p1x, self.__p1y + 145, "white")
             self.get_controlador().draw_text("Curupira", 25, self.__p2x, self.__p2y + 145, "white")
-            self.get_controlador().draw_text("Personagem 3", 25, self.__p3x, self.__p3y + 145, "white")
             self.__botao_voltar = self.get_controlador().draw_text("VOLTAR", 40, self.__voltarx, self.__voltary)
             self.check_input()
             self.draw_cursor()
@@ -59,30 +51,17 @@ class MenuPersonagem(Menu):
     
     def move_cursor(self):
         if self.get_controlador().DOWN_KEY or self.get_controlador().UP_KEY:
-            if self.__state == "Personagem1" or self.__state == "Personagem2" or self.__state == "Personagem3":
+            if self.__state == "Personagem1" or self.__state == "Personagem2":
                 self.get_cursor_rect().midtop = (self.__voltarx + self.__offset_voltar, self.__voltary)
                 self.__state = "Voltar"
             elif self.__state == "Voltar":
                 self.get_cursor_rect().midtop = (self.__p1x + self.__offset_p, self.__p1y)
                 self.__state = "Personagem1"
-        if self.get_controlador().LEFT_KEY:
-            if self.__state == "Personagem1":
-                self.get_cursor_rect().midtop = (self.__p3x + self.__offset_p, self.__p3y)
-                self.__state = "Personagem3"
-            elif self.__state == "Personagem2":
-                self.get_cursor_rect().midtop = (self.__p1x + self.__offset_p, self.__p1y)
-                self.__state = "Personagem1"
-            elif self.__state == "Personagem3":
-                self.get_cursor_rect().midtop = (self.__p2x + self.__offset_p, self.__p2y)
-                self.__state = "Personagem2"
-        if self.get_controlador().RIGHT_KEY:
+        if self.get_controlador().LEFT_KEY or self.get_controlador().RIGHT_KEY:
             if self.__state == "Personagem1":
                 self.get_cursor_rect().midtop = (self.__p2x + self.__offset_p, self.__p2y)
                 self.__state = "Personagem2"
             elif self.__state == "Personagem2":
-                self.get_cursor_rect().midtop = (self.__p3x + self.__offset_p, self.__p3y)
-                self.__state = "Personagem3"
-            elif self.__state == "Personagem3":
                 self.get_cursor_rect().midtop = (self.__p1x + self.__offset_p, self.__p1y)
                 self.__state = "Personagem1"
         if self.get_controlador().MOUSE:
@@ -92,9 +71,6 @@ class MenuPersonagem(Menu):
             if self.__botao_p2.collidepoint(self.get_controlador().MOUSE_POS):
                 self.get_cursor_rect().midtop = (self.__p2x + self.__offset_p, self.__p2y)
                 self.__state = "Personagem2"
-            if self.__botao_p3.collidepoint(self.get_controlador().MOUSE_POS):
-                self.get_cursor_rect().midtop = (self.__p3x + self.__offset_p, self.__p3y)
-                self.__state = "Personagem3"
             elif self.__botao_voltar.collidepoint(self.get_controlador().MOUSE_POS):
                 self.get_cursor_rect().midtop = (self.__voltarx + self.__offset_voltar, self.__voltary)
                 self.__state = "Voltar"
@@ -107,28 +83,18 @@ class MenuPersonagem(Menu):
         if self.__state == "Personagem1":
             self.__cor_borda_p1 = self.__cor_selecionado
             self.__cor_borda_p2 = self.__cor_padrao
-            self.__cor_borda_p3 = self.__cor_padrao
             if (self.__botao_p1.collidepoint(self.get_controlador().MOUSE_POS)
                     and self.get_controlador().MOUSE_CLICK or self.get_controlador().START_KEY):
                 self.__escolhido = 1
         if self.__state == "Personagem2":
             self.__cor_borda_p1 = self.__cor_padrao
             self.__cor_borda_p2 = self.__cor_selecionado
-            self.__cor_borda_p3 = self.__cor_padrao
             if (self.__botao_p2.collidepoint(self.get_controlador().MOUSE_POS)
                     and self.get_controlador().MOUSE_CLICK or self.get_controlador().START_KEY):
                 self.__escolhido = 2
-        if self.__state == "Personagem3":
-            self.__cor_borda_p1 = self.__cor_padrao
-            self.__cor_borda_p2 = self.__cor_padrao
-            self.__cor_borda_p3 = self.__cor_selecionado
-            if (self.__botao_p3.collidepoint(self.get_controlador().MOUSE_POS)
-                    and self.get_controlador().MOUSE_CLICK or self.get_controlador().START_KEY):
-                self.__escolhido = 3
         if self.__state == "Voltar":
             self.__cor_borda_p1 = "white"
             self.__cor_borda_p2 = "white"
-            self.__cor_borda_p3 = "white"
             if (self.__botao_voltar.collidepoint(self.get_controlador().MOUSE_POS)
                     and self.get_controlador().MOUSE_CLICK or self.get_controlador().START_KEY):
                 self.get_cursor_rect().midtop = (self.__p1x + self.__offset_p, self.__p1y)
@@ -139,6 +105,4 @@ class MenuPersonagem(Menu):
     def get_personagem(self):
         if self.__escolhido == 1:
             return self.__personagem1
-        elif self.__escolhido == 2:
-            return self.__personagem2
-        return self.__personagem3
+        return self.__personagem2
